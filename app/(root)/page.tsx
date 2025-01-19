e7a75a9539319e71c2691ed27555907a17145b79
sanjeev0303 <sanjeev.081021@gmail.com>
2025-01-19 08:41:33 +0530

    adding book cover

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,19 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <BookOverview />
+      <BookOverview {...sampleBooks[0]} />
 
-      <BookList />
+      <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+      />
     </>
   );
 };
--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const BookList = () => {
+interface BookListProps {
+    title: string
+    books: Book[]
+    containerClassName?: string
+
+}
+
+const BookList = ({title, books, containerClassName }: BookListProps) => {
   return (
-    <section className='font-bebas-neue text-4xl text-light-100'>Popular Books</section>
+    <section className={containerClassName} >
+        <h2 className='font-bebas-neue text-4xl text-light-100'>{title}</h2>
+
+        <ul className='book-list'>
+            {books.map((book) => (
+                <li>
+                    
+                </li>
+            ))}
+        </ul>
+    </section>
   )
 }
 
